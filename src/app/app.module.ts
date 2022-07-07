a5dda128f4bd059655028c5fa0757fcc24b17cdd
Anisharajar <anisharajar.2002@gmail.com>
2022-07-08 00:22:56 +0530

    department name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StudentsComponent } from './students/students.component';
 import { FacultynameComponent } from './facultyname/facultyname.component';
+import { DepartnameComponent } from './departname/departname.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StudentsComponent,
-    FacultynameComponent
+    FacultynameComponent,
+    DepartnameComponent
   ],
   imports: [
     BrowserModule,
