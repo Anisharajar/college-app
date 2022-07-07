5b4eaf2752e4b02a0d774ab3a92b17fe3d6ee9d4
Anisharajar <anisharajar.2002@gmail.com>
2022-07-08 00:20:16 +0530

    facultyname

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StudentsComponent } from './students/students.component';
+import { FacultynameComponent } from './facultyname/facultyname.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StudentsComponent
+    StudentsComponent,
+    FacultynameComponent
   ],
   imports: [
     BrowserModule,
